433bc820f19778cc89a0a005e3702d5054aeb342
LucaSavini <145345180+LucaSavini@users.noreply.github.com>
2024-02-07 12:32:26 +0100

    Add files via upload

---
+++ Middleware-02/src/main/java/com/example/Middleware02/Configurations/SpringMVCConfig.java
@@ -0,0 +1,17 @@
+package com.example.Middleware02.Configurations;
+
+import com.example.Middleware02.Interceptors.MonthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class SpringMVCConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private MonthInterceptor monthInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(monthInterceptor);
+    }
+}
---
+++ Middleware-02/src/main/java/com/example/Middleware02/Entities/Month.java
@@ -0,0 +1,18 @@
+package com.example.Middleware02.Entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Month {
+    private int monthNumber;
+    private String englishName;
+    private String italianName;
+    private String germanName;
+
+}
